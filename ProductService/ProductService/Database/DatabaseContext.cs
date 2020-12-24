08208b138276b0b85ced405beefd99b8a26ecddf
Suzad <61018495+Suzad@users.noreply.github.com>
2020-12-24 19:38:16 +0600

    Trying

---
+++ ProductService/ProductService/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProductService.Database.Entity;
+
+namespace ProductService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<Product> products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseMySql("server=127.0.0.1;port=3306;user=suzad;password=suzad;database=ProductDB")
+                .UseLoggerFactory(LoggerFactory)
+        }
+    }
+}
---
+++ ProductService/ProductService/Database/Entity/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductService.Database.Entity
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int categoryId { get; set; }
+        public string categoryName { get; set; }
+        public float averageRating { get; set; }
+        public int numberOfRaters { get; set; }
+    }
+}
